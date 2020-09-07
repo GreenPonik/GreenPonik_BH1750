169ed50ee0e44683338ae8fb425e63446465605c
Mickael <mickael.lehoux@greenponik.com>
2020-09-07 17:08:43 +0200

    give access to change i2c bus

--- GreenPonik_BH1750/GreenPonik_BH1750.py
+++ GreenPonik_BH1750/GreenPonik_BH1750.py
@@ -16,42 +16,45 @@ import busio
 
 class GreenPonik_BH1750:
     # Constants taken from the datasheet
-    DEVICE = 0x23       # Default device I2C address
-    POWER_DOWN = 0x00   # No active state
-    POWER_ON = 0x01     # Power on
-    RESET = 0x07        # Reset data register value
+    DEFAULT_ADDR = 0x23       # Default device I2C address
+    _POWER_DOWN = 0x00   # No active state
+    _POWER_ON = 0x01     # Power on
+    _RESET = 0x07        # Reset data register value
     # Start measurement at 4 lx resolution. Time typically 16ms.
-    CONTINUOUS_LOW_RES_MODE = 0x13
+    _CONTINUOUS_LOW_RES_MODE = 0x13
     # Start measurement at 1 lx resolution. Time typically 120ms
-    CONTINUOUS_HIGH_RES_MODE_1 = 0x10
+    _CONTINUOUS_HIGH_RES_MODE_1 = 0x10
     # Start measurement at 0.5 lx resolution. Time typically 120ms
-    CONTINUOUS_HIGH_RES_MODE_2 = 0x11
+    _CONTINUOUS_HIGH_RES_MODE_2 = 0x11
     # Start measurement at 1 lx resolution. Time typically 120ms
     # Device is automatically set to Power Down after measurement.
-    ONE_TIME_HIGH_RES_MODE_1 = 0x20
+    _ONE_TIME_HIGH_RES_MODE_1 = 0x20
     # Start measurement at 0.5 lx resolution. Time typically 120ms
     # Device is automatically set to Power Down after measurement.
-    ONE_TIME_HIGH_RES_MODE_2 = 0x21
+    _ONE_TIME_HIGH_RES_MODE_2 = 0x21
     # Start measurement at 1 lx resolution. Time typically 120ms
     # Device is automatically set to Power Down after measurement.
-    ONE_TIME_LOW_RES_MODE = 0x23
+    _ONE_TIME_LOW_RES_MODE = 0x23
+
+    def __init__(self, scl_pin=None, sda_pin=None):
+        self._scl_pin = scl_pin if not None else board.SCL
+        self._sda_pin = sda_pin if not None else board.SDA
 
     def _convert_to_number(self, data):
         # Simple function to convert 2 bytes of data
         # into a decimal number
         return ((data[1] + (256 * data[0])) / 1.2)
 
-    def read_bh1750(self, addr=DEVICE):
+    def read_bh1750(self, addr=DEFAULT_ADDR):
         try:
             # bus = smbus.SMBus(0)    # Rev 1 Pi uses 0
             # bus = smbus.SMBus(1)    # Rev 2 Pi uses 1
             # data = bus.read_i2c_block_data(addr, \
-            # self.ONE_TIME_HIGH_RES_MODE_2)
+            # self._ONE_TIME_HIGH_RES_MODE_2)
             # lux = self._convertToNumber(data)
-            i2c = busio.I2C(board.SCL, board.SDA)
+            i2c = busio.I2C(self._scl_pin, self._sda_pin)
             buffer = bytearray(1)
             bh = i2c.readfrom_into(addr, buffer)
-            print(bh)
             lux = bh
             print('light: %.3f lx' % lux)
             i2c.deinit()
