30e07a7b1832dcff140e92d9a87a38ad5f5749a3
Mickael <mickael.lehoux@greenponik.com>
2020-09-08 09:58:52 +0200

    b

--- GreenPonik_BH1750/GreenPonik_BH1750.py
+++ GreenPonik_BH1750/GreenPonik_BH1750.py
@@ -10,43 +10,50 @@
 ####################################################################
 """
 
-import board
-import busio
+from adafruit_extended_bus import ExtendedI2C as I2C
+# import board
+# import busio
 
 
 class GreenPonik_BH1750:
     # Constants taken from the datasheet
+    DEFAULT_BUS = 1
     DEFAULT_ADDR = 0x23       # Default device I2C address
-    _POWER_DOWN = 0x00   # No active state
-    _POWER_ON = 0x01     # Power on
-    _RESET = 0x07        # Reset data register value
-    # Start measurement at 4 lx resolution. Time typically 16ms.
-    _CONTINUOUS_LOW_RES_MODE = 0x13
-    # Start measurement at 1 lx resolution. Time typically 120ms
-    _CONTINUOUS_HIGH_RES_MODE_1 = 0x10
-    # Start measurement at 0.5 lx resolution. Time typically 120ms
-    _CONTINUOUS_HIGH_RES_MODE_2 = 0x11
-    # Start measurement at 1 lx resolution. Time typically 120ms
-    # Device is automatically set to Power Down after measurement.
-    _ONE_TIME_HIGH_RES_MODE_1 = 0x20
-    # Start measurement at 0.5 lx resolution. Time typically 120ms
-    # Device is automatically set to Power Down after measurement.
-    _ONE_TIME_HIGH_RES_MODE_2 = 0x21
-    # Start measurement at 1 lx resolution. Time typically 120ms
-    # Device is automatically set to Power Down after measurement.
-    _ONE_TIME_LOW_RES_MODE = 0x23
+    # _POWER_DOWN = 0x00   # No active state
+    # _POWER_ON = 0x01     # Power on
+    # _RESET = 0x07        # Reset data register value
+    # # Start measurement at 4 lx resolution. Time typically 16ms.
+    # _CONTINUOUS_LOW_RES_MODE = 0x13
+    # # Start measurement at 1 lx resolution. Time typically 120ms
+    # _CONTINUOUS_HIGH_RES_MODE_1 = 0x10
+    # # Start measurement at 0.5 lx resolution. Time typically 120ms
+    # _CONTINUOUS_HIGH_RES_MODE_2 = 0x11
+    # # Start measurement at 1 lx resolution. Time typically 120ms
+    # # Device is automatically set to Power Down after measurement.
+    # _ONE_TIME_HIGH_RES_MODE_1 = 0x20
+    # # Start measurement at 0.5 lx resolution. Time typically 120ms
+    # # Device is automatically set to Power Down after measurement.
+    # _ONE_TIME_HIGH_RES_MODE_2 = 0x21
+    # # Start measurement at 1 lx resolution. Time typically 120ms
+    # # Device is automatically set to Power Down after measurement.
+    # _ONE_TIME_LOW_RES_MODE = 0x23
 
-    def __init__(self, scl_pin=None, sda_pin=None):
-        self._scl_pin = scl_pin if None is not scl_pin else board.SCL
-        self._sda_pin = sda_pin if None is not sda_pin else board.SDA
-
-    @property
-    def scl_pin(self):
-        return self._scl_pin
+    def __init__(self, bus=None):
+        # self._scl_pin = scl_pin if None is not scl_pin else board.SCL
+        # self._sda_pin = sda_pin if None is not sda_pin else board.SDA
+        self._bus = bus if None is not bus else self.DEFAULT_BUS
 
     @property
-    def sda_pin(self):
-        return self._sda_pin
+    def bus(self):
+        return self._bus
+
+    # @property
+    # def scl_pin(self):
+    #     return self._scl_pin
+
+    # @property
+    # def sda_pin(self):
+    #     return self._sda_pin
 
     def _convert_to_number(self, data):
         # Simple function to convert 2 bytes of data
@@ -60,11 +67,12 @@ class GreenPonik_BH1750:
             # data = bus.read_i2c_block_data(addr, \
             # self._ONE_TIME_HIGH_RES_MODE_2)
             # lux = self._convertToNumber(data)
-            i2c = busio.I2C(self._scl_pin, self._sda_pin)
+            # i2c = busio.I2C(self._scl_pin, self._sda_pin)
+            i2c = I2C(self._bus)
             buffer = bytearray(1)
             bh = i2c.readfrom_into(addr, buffer)
             lux = bh
-            print('light: %.3f lx' % lux)
+            print('Light: %.3f lx' % lux)
             i2c.deinit()
             return lux
         except BaseException as e:
